8c8cdddbdb4dc79ed34faca4a8be67ec5af72294
M96820 <estienneg@theodo.fr>
2025-01-24 21:22:38 +0100

    clean: delete all useless functions

--- ai-comic-factory/src/app/queries/getLLMEngineFunction.ts
+++ ai-comic-factory/src/app/queries/getLLMEngineFunction.ts
@@ -1,19 +1,6 @@
-import { LLMEngine } from "@/types"
-import { predict as predictWithHuggingFace } from "./predictWithHuggingFace"
-import { predict as predictWithOpenAI } from "./predictWithOpenAI"
-import { predict as predictWithGroq } from "./predictWithGroq"
-import { predict as predictWithAnthropic } from "./predictWithAnthropic"
+import { LLMEngine } from '@/types';
+
+import { predict as predictWithHuggingFace } from './predictWithHuggingFace';
 
 export const defaultLLMEngineName = `${process.env.LLM_ENGINE || ""}` as LLMEngine
-
-export function getLLMEngineFunction(llmEngineName: LLMEngine = defaultLLMEngineName) {
-  const llmEngineFunction = 
-    llmEngineName === "GROQ" ? predictWithGroq :
-    llmEngineName === "ANTHROPIC" ? predictWithAnthropic :
-    llmEngineName === "OPENAI" ? predictWithOpenAI :
-    predictWithHuggingFace
-  
-  return llmEngineFunction
-}
-
-export const defaultLLMEngineFunction = getLLMEngineFunction()
+export const defaultLLMEngineFunction = predictWithHuggingFace
