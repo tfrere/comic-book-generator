fdc200aeb653e7d7d55a3d0dcc1ac1e81b955c6b
M96820 <estienneg@theodo.fr>
2025-01-24 20:16:43 +0100

    feat: add ai-comic-factory

---
+++ ai-comic-factory/src/app/interface/discord/index.tsx
@@ -0,0 +1,20 @@
+import { FaDiscord } from "react-icons/fa"
+
+
+export function Discord() {
+  return (
+    <a
+      className="
+      flex flex-row items-center justify-center
+      h-10
+      no-underline
+      animation-all duration-150 ease-in-out
+      text-stone-700 hover:text-stone-950 scale-95 hover:scale-100"
+      href="https://discord.gg/AEruz9B92B"
+      target="_blank">
+      <div><FaDiscord size={24} /></div>
+      <span className="text-sm ml-1.5 hidden md:inline">Discord</span>
+      <span className="text-sm ml-1.5 inline md:hidden"></span>
+    </a>
+  )
+}
